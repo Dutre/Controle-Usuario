92a1bb7f1615bff63d2e3792f1596cf76137284d
=renan-dutre@hotmail.com <=renan-dutre@hotmail.com>
2017-03-13 22:12:11 -0400

    Adicionado métodos deletar e alterar

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -34,6 +34,49 @@ public class UsuarioController extends HttpServlet {
 		resp.getWriter().println("{ nome:" + usu.getNome() + "  , email: " + usu.getEmail() + "  } ");
 
 	}
+	
+	
+	
+	/*
+	 * Deletar por nome e email.
+	 * 
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String nome = req.getParameter("nome");
+		String email = req.getParameter("email");
+		
+		for (int i = 0; i < lista.size(); i++) {
+			if(lista.get(i).getNome().equals(nome)|| lista.get(i).getEmail().equals(email) ){
+				lista.remove(i);
+				break;
+			}
+			
+		}
+	}
+	*/
+	
+	// Deletar por índice.
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int op = Integer.parseInt(req.getParameter("op"));
+		
+		lista.remove(op);
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// Indice a ser alterado
+		int op = Integer.parseInt(req.getParameter("op"));
+
+		
+		// Dados a serem alterados
+		String nome = req.getParameter("nome");
+		String email = req.getParameter("email");
+		
+		//Acessando objeto e alterando dados
+		Usuario usu = lista.get(op);
+		usu.setNome(nome);
+		usu.setEmail(email);
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
